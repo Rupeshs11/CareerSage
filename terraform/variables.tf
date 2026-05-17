9e4c18f335a3df58f7259f1c6f81bab8776f3e5f
rupeshs11 <rbsatale7@gmail.com>
2026-05-17 23:12:25 +0530

    chore: upgrade instance type to m7i.xlarge for final presentation

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,7 +11,7 @@ variable "aws_region" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t3.micro"
+  default     = "m7i.xlarge"
 }
 
 variable "ami_id" {
